89f9a1c11c26ef087d5e23b42b4e480f111eebb9
borisova <borisova.marie1@gmail.com>
2024-05-24 11:29:09 +0200

    Edited some text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
-<head>Book Report Management</head>
+<meta charset="utf-8">
+<meta name = "viewport" content="width = device-width, initial-scale = 1">
 
+<style>
+  header{
+    color:brown
+  }
+</style>
+
+
+<header>
+  <h1>
+  Book Report Management
+  </h1>
+</header>
 <body>
   <h1><i>Book Report Management</i></h1>
 </body><h2>hello</h2>
