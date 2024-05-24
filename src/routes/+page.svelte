92782b21e0c8463aa099d00fec1e33ec7c5baf5a
Domysli si <s.maj683@gmail.com>
2024-05-24 10:54:59 +0200

    c

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
+<head>Book Report Management</head>
 
-
-<h1>Welcome to SvelteKit</h1>
-
-<h1>Book Report Management</h1>
-
+<body>
+  <h1>Book Report Management</h1>
+</body>
 <h2>hello</h2>
 
 <h2>POMOC!</h2>
-
