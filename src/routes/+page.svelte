9338fef5c96a22562023795db74e5758980bfbc5
borisova <borisova.marie1@gmail.com>
2024-05-24 13:40:50 +0200

    Changed background color and font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
+<script>
+
+    import { build } from "$service-worker";
+
+</script>
 <head>
 <meta charset="utf-8">
 <meta name = "viewport" content="width = device-width, initial-scale = 1">
 <meta name="viewport" content="width=device-width, initial-scale = 1.0">
 
 <style>
+  body{
+    background-color: #efa00b
+  }
   header{
-    background-color: darkseagreen;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: Monospace;
+    font-size: 200%;
     text-align: center;
-    color:brown
+    color:#0267c1
+    
+  }
+
+  section{
+    color: #591f0a;
+    font-family: Monospace;
+  
+  
+
   }
 </style>
 
@@ -18,17 +35,19 @@
 <body>
   <header>
     <h1>
-    Book Report Management
+    [Book Report Management]
     </h1>
   </header>
 
   <section>
     <nav>
       <ul>
-        <li>World and Czech literature till the end of the 18th century</li>
-        <li>World and Czech literature till the end of the 19th century</li>
-        <li>World literature - 20th and 21st century </li>
-        <li>Czech literature - 20th and 21st century </li>
+        <b>
+        <p>World and Czech literature till the end of the 18th century</p>
+        <p>World and Czech literature till the end of the 19th century</p>
+        <p>World literature - 20th and 21st century </p>
+        <p>Czech literature - 20th and 21st century </p>
+        </b>
       </ul>
     </nav>
   </section>
