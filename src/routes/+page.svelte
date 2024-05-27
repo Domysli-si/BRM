3b3a073f529166ef767aab3767211aa2a9f78b84
Domysli si <s.maj683@gmail.com>
2024-05-27 10:48:43 +0200

    Update +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+
+    import { build } from "$service-worker";
+
+</script>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale = 1.0">
+
+<style>
+  body{
+    background-color: #efa00b
+  }
+  header{
+    font-family: Monospace;
+    font-size: 200%;
+    text-align: center;
+    color:#0267c1
+    
+  }
+
+  section{
+    color: #591f0a;
+    font-family: Monospace;
+  
+  
+
+  }
+</style>
+
+
+
+</head>
+<body>
+  <header>
+    <h1>
+    [Book Report Management]
+    </h1>
+  </header>
+
+  <section>
+    <nav>
+      <ul>
+        <b>
+        <p>World and Czech literature till the end of the 18th century</p>
+        <p>World and Czech literature till the end of the 19th century</p>
+        <p>World literature - 20th and 21st century </p>
+        <p>Czech literature - 20th and 21st century </p>
+        </b>
+      </ul>
+    </nav>
+  </section>
+</body>
